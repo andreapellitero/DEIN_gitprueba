cfe2d730b962b01250295de91ccbf11b4af42881
andreapellitero <andrea.pelcas.1@gmail.com>
2021-12-13 19:36:44 +0100

    Añadir clase principal

---
+++ src/main/java/com/mycompany/pruebagit/HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pruebagit;
+
+/**
+ *
+ * @author andre
+ */
+public class HelloWorld extends  javax.swing.JFrame{
+    public static void main (String []args){
+        HelloWorld app = new HelloWorld();
+        app.setSize(300,200);
+        app.setTitle("Hola Mundo");
+        app.setVisible(true);
+    }
+    
+}
